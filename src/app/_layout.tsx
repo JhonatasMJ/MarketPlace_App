1fbff08840f8d287a57dd684a775a9a67ebc2ffb
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-05-11 14:15:17 -0300

    feat/config axios and react-query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,14 @@
 import { Stack } from "expo-router"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import "../styles/global.css"
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}/>
+    <QueryClientProvider client={queryClient}>
+       <Stack screenOptions={{ headerShown: false }}/> 
+    </QueryClientProvider>
+   
   )
 }

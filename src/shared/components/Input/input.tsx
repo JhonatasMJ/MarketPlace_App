86058f3ceaaad70899cd5c45180aac2868d3033b
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-05-15 18:38:12 -0300

    feat/input styles

--- src/shared/components/Input/input.tsx
+++ src/shared/components/Input/input.tsx
@@ -1,18 +1,26 @@
-import { Pressable, TextInput, TouchableOpacity, View } from "react-native"
-import { inputVariants } from "./input.variants"
+import { Pressable, Text, TextInput, TextInputProps, TouchableOpacity, View } from "react-native"
+import { inputVariants, InputVariantsProps } from "./input.variants"
 import {Ionicons} from '@expo/vector-icons'
 
-export function Input() { 
+export interface InputProps extends TextInputProps,InputVariantsProps{
+    label?: string;
+    leftIcon?: keyof typeof Ionicons.glyphMap;
+    containerClassName?: string;
+    mask?: (value: string) => void | string;
+}
+
+export function Input({label, leftIcon, containerClassName,className, mask, ...props}: InputProps) { 
     const styles = inputVariants({
     
     })
     return (
-        <View>
-            <Pressable>
-                <Ionicons name="eye-outline" size={24} color="gray" />
-                <TextInput/>
+        <View className={styles.container({className: containerClassName})}>
+            <Text className={styles.label()}>Label</Text>
+            <Pressable className={styles.wrapper()}>
+                <Ionicons name="person" size={22} color="gray" />
+                <TextInput className={styles.input({className: className})} {...props}/>
                 <TouchableOpacity>
-                    <Ionicons name="eye-off-outline" size={24} color="gray" />
+                    <Ionicons name="eye-off-outline" size={22} color="gray" />
                 </TouchableOpacity>
             </Pressable>
         </View>

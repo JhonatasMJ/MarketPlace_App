8b7b89520d43dfaf65af6526d18471fbae36d767
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-05-22 14:06:46 -0300

    feat/users persistence

--- src/shared/queries/auth/use-login.mutation.ts
+++ src/shared/queries/auth/use-login.mutation.ts
@@ -1,14 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import * as authService from "../../services/auth.service";
 import { LoginHttpParams } from "../../interfaces/http/login";
+import { useUserStore } from "../../store/user-store";
 
 /* Essa função é responsável por criar uma instância do useMutation do tanstack/react-query de login de usuário, pegando do service de autenticação a função de login, ja retorna um loading, sucesso e erro */
 export const useLoginMutation = () => {
+
+  const {setSession} = useUserStore()
+
   const mutation = useMutation({
     mutationFn: (userData: LoginHttpParams) =>
       authService.login(userData),
     onSuccess: (response) => {
-      console.log(response);
+      setSession(response)
     },
     onError: (error) => {
       console.log(error);

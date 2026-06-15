4fe0a09889de4d772a37f367fe9c373a6e4a3e1a
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-06-15 13:58:10 -0300

    feat/products details

--- src/shared/components/ProductHeader/index.tsx
+++ src/shared/components/ProductHeader/index.tsx
@@ -4,6 +4,8 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { colors } from "../../../styles/colors";
 import { buildImageUrl } from "../../helpers/buildImageUrl";
+import { PriceText } from "../PriceText";
+import { router } from "expo-router";
 
 interface ProductHeaderParams {
   productDetails: GetProductDetailInterface;
@@ -13,12 +15,12 @@ export const ProductHeader: FC<ProductHeaderParams> = ({ productDetails }) => {
   return (
     <>
       <View className="pb-5 items-start">
-        <TouchableOpacity className="w-full justify-start flex-row items-center gap-3">
+        <TouchableOpacity onPress={() => router.back()} className="w-full justify-start flex-row items-center gap-3">
           <Ionicons name="arrow-back" size={24} color={colors["purple-base"]} />
           <Text className="text-base text-purple-base">Voltar</Text>
         </TouchableOpacity>
       </View>
-      <View className="w-full rounded-lg shadow-xl shadow-gray-500/30 bg-white">
+      <View className="w-full rounded-lg  shadow-gray-500/30 bg-white">
         <Image
           source={{ uri: buildImageUrl(productDetails.photo) }}
           className="w-full rounded-lg h-[192px]"
@@ -31,6 +33,64 @@ export const ProductHeader: FC<ProductHeaderParams> = ({ productDetails }) => {
           <Text className="text-[10px] font-semibold text-gray-800 ml-1">
             / 5
           </Text>
+        </View>
+      </View>
+      <View className="bg-background py-8 ">
+        <View className="flex-row justify-between items-center mb-4">
+          <Text className="text-xl font-bold text-gray-800 max-w-[70%]">
+            {productDetails.name}
+          </Text>
+          <View>
+            <PriceText
+              value={Number(productDetails.value)}
+              classNameValue="text-2xl font-bold text-gray-800 ml-1"
+            />
+          </View>
+        </View>
+        <View className="flex-row items-center bg-blue-light p-3 mb-4 rounded-lg">
+          <View className="bg-blue-base w-[36px] h-[36px] rounded-[6px] items-center justify-center">
+            <Ionicons name="trending-up" size={20} color={colors.white} />
+          </View>
+          <Text className="text-sm text-gray-600 ml-5 flex-1">
+            <Text className="font-bold">{productDetails.views} pessoas </Text>
+            visualizaram este produto nos últimos 7 dias.
+          </Text>
+        </View>
+        <View className="mb-4">
+          <Text className="text-base leading-6 text-gray-500">
+            {productDetails.description}{" "}
+          </Text>
+        </View>
+        {(productDetails.width || productDetails.height) && (
+          <View className="mb-4">
+            {productDetails.width && (
+              <Text className="text-base text-gray-500 mb-1">
+                <Text className="text-gray-800">Largura:</Text>{" "}
+                {productDetails.width}
+              </Text>
+            )}
+
+            {productDetails.height && (
+              <Text className="text-base text-gray-500 mb-1">
+                <Text className="text-gray-800">Altura:</Text>{" "}
+                {productDetails.height}
+              </Text>
+            )}
+          </View>
+        )}
+        <View className="mb-6">
+          <Text className="text-base font-bold text-gray-800 ">Categoria</Text>
+          <Text className="text-base text-gray-600">
+            {productDetails.category.name}
+          </Text>
+        </View>
+        <View className="flex-row justify-between items-center py-4 border-t border-gray-200">
+          <Text className="text-lg font-bold text-gray-800">Avaliações</Text>
+          <TouchableOpacity>
+            <Text className="text-purple-base text-base font-medium">
+              Avaliar
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </>

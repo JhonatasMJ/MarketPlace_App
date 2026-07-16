ea6b2be19a9fbb46027418f8c9d72f58c39e08f6
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-07-16 11:41:11 -0300

    feat/ListHeader Orders

--- src/viewModels/Orders/Orders.view.tsx
+++ src/viewModels/Orders/Orders.view.tsx
@@ -4,16 +4,18 @@ import { FlatList} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { OrderItem } from "./components/OrderItem/OrderItem";
 import { EmptyList } from "./components/EmptyList/EmptyList";
+import { ListHeader } from "./components/ListHeader/ListHeader";
 
 export const OrdersView: FC<ReturnType<typeof useOrdersViewModel>> = ({ orders }) => {
     return (
-        <SafeAreaView className="flex-1">
+        <SafeAreaView edges={["top"]} className="flex-1">
           <FlatList
             contentContainerClassName="px-[16px] pb-[120px]"
             data={orders}
             renderItem={({ item }) => <OrderItem order={item} />}
             keyExtractor={({id}) => `order-${id}`}
             ListEmptyComponent={<EmptyList />}
+            ListHeaderComponent={<ListHeader />}
           />
         </SafeAreaView>
     )
